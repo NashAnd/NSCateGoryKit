4afb7654f9113e280f54e1be0dc8ab0d1dad693d
admin <1252419379@qq.com>
2019-05-16 14:16:20 +0800

    新的修改

--- Example/NSCateGoryKit/NSViewController.m
+++ Example/NSCateGoryKit/NSViewController.m
@@ -7,7 +7,6 @@
 //
 
 #import "NSViewController.h"
-
 @interface NSViewController ()
 
 @end
@@ -18,6 +17,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+  
 }
 
 - (void)didReceiveMemoryWarning
---
+++ Example/Pods/Target Support Files/NSCateGoryKit/NSCateGoryKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_NSCateGoryKit : NSObject
+@end
+@implementation PodsDummy_NSCateGoryKit
+@end
